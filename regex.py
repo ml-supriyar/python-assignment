e45e24be5db5aa585ee8f0aae8a30685f8791b6c
ml-supriyar <87848277+ml-supriyar@users.noreply.github.com>
2021-08-17 14:07:35 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,46 +1,65 @@
-# Import the library
-import re
+import re 
 import sys
 import os.path
 import argparse
 
 
-# Function to print filename, line number and the line
-def print_msg(file_name,line_number,pattern,line,machine):
+def print_msg(file_name,line_number,pattern,line,machine,start_pos):
     try:
         machine=int(machine)
+    except:
+        print("Please enter 0 or 1 for -m flag")
+        return
     if machine==1:
-        print("{}:{}:{}".format(file_name,line_number,pattern))
-    else:
+        print("{}:{}:{}:{}".format(file_name,line_number,start_pos,pattern))
+    elif machine==0:
         print("{}:{}".format(file_name,line))
+    else:
+        print("Please enter 0 or 1 for -m flag")
 
 
-# Regex functionn to search pattern in a file
+
+
 def regex_search(path,pattern,machine):
     file_exists=os.path.isfile(path)
     if file_exists==False:
+        print("file not found, enter correct file name")
         return
     ipfile = open(path,"r")
     lines=ipfile.readlines()
     search_pattern= re.compile(pattern)
- 
-
-# Print the line number if the pattern exist
     for counter, line in enumerate(lines):
-        ab = search_pattern.search(line)
-        boolean = bool(ab)
-        if boolean:
-            print_msg(path,counter+1,pattern,line,machine)
-            
-            
-#  Create the parser , Add and Parse the arguments
-if __name__ == "__main__":
+        for m in search_pattern.finditer(line):
+            start_pos=m.start()
+            if start_pos==0 or start_pos:
+                print_msg(path,counter+1,pattern,line,machine,start_pos)
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--regex", help="Pattern")
     parser.add_argument("-f", "--files", help="file name")
-    parser.add_argument("-m", "--machine", help="file name")
+    parser.add_argument("-m", "--machine", help="machine readable")
     args = parser.parse_args()
     path = args.files
     machine=args.machine
-    pattern = args.regex
-    regex_search(path,pattern,machine)
+    pattern=args.regex
+    if machine==None:
+        machine=0
+    try:
+        machine=int(machine)
+    except:
+        print("Please enter 0 or 1 for -m flag")
+        return
+    if machine==1 or machine==0:
+        try:
+            regex_search(path,pattern,machine)
+        except:
+            print("please enter filename with f flag and pattern with -r flag")
+            return
+    else:
+        print("Please enter 0 or 1 for -m flag")
+        return
+
+
+if __name__ == "__main__":
+    main()
