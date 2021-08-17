aa5d531cf013ef864e434b592c7ead182760db5b
ml-supriyar <87848277+ml-supriyar@users.noreply.github.com>
2021-08-17 15:41:44 +0530

    Update regex_match.py

--- regex_match.py
+++ regex_match.py
@@ -16,10 +16,11 @@ def print_msg(file_name,line_number,pattern,line,machine,start_pos):
         print("Please enter 0 or 1 for -m flag")
 
 
-# Checks if the file exists
+# Checks if the given patterns exists in the file
 def regex_search(path,pattern,machine):
+# Check if the file exist
     file_exists=os.path.isfile(path)
-# If the file doest exists then print the error msg
+# If the file doest exists then print the error msg and return
     if file_exists==False:
         print("File not found, Enter correct file name")
         return
@@ -28,22 +29,23 @@ def regex_search(path,pattern,machine):
 # Reads the lines and stores it as a list
     lines=ipfile.readlines()
     search_pattern= re.compile(pattern)
-# Counts each and every line in the file
+# Iterate through each element of the list
     for counter, line in enumerate(lines):
 # Finditer scans the string from left to right, and matches are returned in the iterator form.
         for m in search_pattern.finditer(line):
-# Points sarting index of the match and calls if condition whenever match == 0 or any value
+# Points starting index of the match and calls print function whenever there is a match
             start_pos=m.start()
             if start_pos==0 or start_pos:
                 print_msg(path,counter+1,pattern,line,machine,start_pos)
 
                 
-# Calls main function and takes the input argument          
 def main():
+# Taking the inputs with -m , -r , -f flags
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--regex", help="Pattern")
     parser.add_argument("-f", "--files", help="file name")
     parser.add_argument("-m", "--machine", help="machine readable")
+# Assigning user given input to variables
     args = parser.parse_args()
     path = args.files
     machine=args.machine
@@ -51,7 +53,7 @@ def main():
 # Handles error  , if machine is none then it will be initialized to zero
     if machine==None:
         machine=0
-# Handles error if the input is not an integer 
+# Handles error if the machine input is not an integer 
     try:
         machine=int(machine)
     except:
@@ -61,7 +63,7 @@ def main():
     if machine==1 or machine==0:
         try:
             regex_search(path,pattern,machine)
-# If path and patern is empty then it will throw error
+# If path and patern is empty then it will return with print msg
         except:
             print("please enter filename with f flag and pattern with -r flag")
             return
