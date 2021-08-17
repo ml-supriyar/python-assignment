f63a2b7bad4a9c7d99104d276327d670b647a057
ml-supriyar <87848277+ml-supriyar@users.noreply.github.com>
2021-08-17 14:41:54 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,15 +1,18 @@
+#Import Libraries
 import re 
 import sys
 import os.path
 import argparse
 
-
+# Call function and print the expected Output
 def print_msg(file_name,line_number,pattern,line,machine,start_pos):
-    try:
+# Handles error if the input is not an integer 
+try:
         machine=int(machine)
     except:
         print("Please enter 0 or 1 for -m flag")
         return
+# Print machine readable output if value is 1
     if machine==1:
         print("{}:{}:{}:{}".format(file_name,line_number,start_pos,pattern))
     elif machine==0:
@@ -19,21 +22,28 @@ def print_msg(file_name,line_number,pattern,line,machine,start_pos):
 
 
 
-
+# Checks if the file exists
 def regex_search(path,pattern,machine):
     file_exists=os.path.isfile(path)
+# If the file doest exists then print the error msg
     if file_exists==False:
-        print("file not found, enter correct file name")
+        print("File not found, Enter correct file name")
         return
+# Open the file using the given path
     ipfile = open(path,"r")
+# Reads the lines and stores it as a list
     lines=ipfile.readlines()
     search_pattern= re.compile(pattern)
+# Counts each and every line in the file
     for counter, line in enumerate(lines):
+# Finditer scans the string from left to right, and matches are returned in the iterator form.
         for m in search_pattern.finditer(line):
+# Points sarting index of the match and calls if condition whenever match == 0 or any value
             start_pos=m.start()
             if start_pos==0 or start_pos:
                 print_msg(path,counter+1,pattern,line,machine,start_pos)
 
+# Calls main function and takes the input argument          
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--regex", help="Pattern")
@@ -43,6 +53,7 @@ def main():
     path = args.files
     machine=args.machine
     pattern=args.regex
+# Handles error
     if machine==None:
         machine=0
     try:
